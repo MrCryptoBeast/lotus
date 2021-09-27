3d586ed3c601b8ba35b4a4b07ffb45bc521cc2a9
pxx <pxx888@gmail.com>
2021-09-27 14:45:14 +0800

    add calibnet testnet

--- build/drand.go
+++ build/drand.go
@@ -32,16 +32,12 @@ const (
 var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 	DrandMainnet: {
 		Servers: []string{
-			"https://api.drand.sh",
-			"https://api2.drand.sh",
-			"https://api3.drand.sh",
+			"https://api1.drand.top",
+			"https://api2.drand.top",
+			"https://api3.drand.top",
 		},
-		Relays: []string{
-			"/dnsaddr/api.drand.sh/",
-			"/dnsaddr/api2.drand.sh/",
-			"/dnsaddr/api3.drand.sh/",
-		},
-		ChainInfoJSON: `{"public_key":"868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31","period":30,"genesis_time":1595431050,"hash":"8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce","groupHash":"176f93498eac9ca337150b46d21dd58673ea4e3581185f869672e59fa4cb390a"}`,
+		Relays:        []string{},
+		ChainInfoJSON: `{"public_key":"802b72507592ecf040e7c95a9d96d9e950b0dd426be36c62e2cdf3d4cb2a307acb5efa7c2fe222087e450f6e9ccc5257","period":30,"genesis_time":1602883530,"hash":"977236ee92f8edff9e28ab68bd0c5849be5226800a344e490267dd9950b273f3","groupHash":"18d8227dad40642b74463a492b37d43e1a8d3149710383d8cd5dadd2b241b2c4"}`,
 	},
 	DrandTestnet: {
 		Servers: []string{
--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -10,43 +10,49 @@ import (
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,
+	0: DrandIncentinet,
 }
 
 const BootstrappersFile = "calibnet.pi"
 const GenesisFile = "calibnet.car"
 
-const UpgradeBreezeHeight = -1
+const UpgradeCreeperHeight = 8000
+const UpgradeBreezeHeight = 10000
 const BreezeGasTampingDuration = 120
 
-const UpgradeSmokeHeight = -2
+const UpgradeSmokeHeight = 11800
 
-const UpgradeIgnitionHeight = -3
-const UpgradeRefuelHeight = -4
+const UpgradeIgnitionHeight = 12400
+const UpgradeRefuelHeight = 13000
+const UpgradeHogwartsHeight = 13600
+const UpgradeSiriusHeight = 14200
 
-var UpgradeStableHeight = abi.ChainEpoch(25)
-var UpgradeActorsV2Height = abi.ChainEpoch(30)
+var UpgradeStableHeight = abi.ChainEpoch(14800)
+var UpgradeActorsV2Height = abi.ChainEpoch(10_000_001)
 
-const UpgradeTapeHeight = 60
+const UpgradeTapeHeight = 10_000_002
 
-const UpgradeLiftoffHeight = -5
+// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
+// Miners, clients, developers, custodians all need time to prepare.
+// We still have upgrades and state changes to do, but can happen after signaling timing here.
+const UpgradeLiftoffHeight = 10_000_003
+const UpgradeKumquatHeight = 10_000_004
+const UpgradeCalicoHeight = 10_000_005
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
 
-const UpgradeKumquatHeight = 90
+const UpgradeOrangeHeight = UpgradePersianHeight + 1
 
-const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
+// 2020-12-22T02:00:00Z
+const UpgradeClausHeight = UpgradeOrangeHeight + 1
 
-const UpgradeClausHeight = 250
-
-const UpgradeOrangeHeight = 300
-
-const UpgradeActorsV3Height = 600
+// 2021-03-04T00:00:30Z
+var UpgradeActorsV3Height = abi.ChainEpoch(UpgradeClausHeight + 1)
 
 func init() {
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(20 << 30))
 	policy.SetSupportedProofTypes(
-		abi.RegisteredSealProof_StackedDrg32GiBV1,
-		abi.RegisteredSealProof_StackedDrg64GiBV1,
+		abi.RegisteredSealProof_StackedDrg16GiBV1,
+		abi.RegisteredSealProof_StackedDrg4GiBV1,
 	)
 
 	SetAddressNetwork(address.Testnet)
@@ -61,4 +67,4 @@ const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4
+const BootstrapPeerThreshold = 1
